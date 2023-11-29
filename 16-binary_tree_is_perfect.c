2037edbaff2603b73e09f0064238ffb782965026
habtew16 <habtamuwolde96@gmail.com>
2023-11-29 10:57:43 -0800

    fixed checker error in binary tree is perect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,42 +1,32 @@
 #include "binary_trees.h"
-#include "13-binary_tree_nodes.c"
+
 
 
 /**
- * max - function to find max of two
- * numbers
- * @a: first number
- * @b: second number
+ * is_perfect - function to checl if the
+ * tree is perfect
  *
- * Return: return largest
+ * @tree: tree
+ * Return: 0 if is not
+ * else 1
  */
-
-int max(int a, int b)
+int is_perfect(const binary_tree_t *tree)
 {
-	return ((a > b) ? a : b);
+	int l_tree = 0;
+	int r_tree = 0;
+
+	if (tree->left && tree->right)
+	{
+		l_tree = 1 + is_perfect(tree->left);
+		r_tree = 1 + is_perfect(tree->right);
+		if (r_tree == l_tree && r_tree != 0 && l_tree != 0)
+			return (r_tree);
+		return (0);
+	}
+	else if (!tree->left && !tree->right)
+		return (1);
+	return (0);
 }
-
-
-/**
- * binary_tree_height - calculate the height of the
- * binary tree
- * @tree: bbinary tree
- * Return: 0 if null else
- * height of the tree
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t l_height;
-	size_t r_height;
-
-	if (tree == NULL)
-		return (-1);
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	return (1 + max(l_height, r_height));
-}
-
 
 /**
  * binary_tree_is_perfect - function to check if binary tree
@@ -48,18 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height;
-	int node;
-	int count;
-	int i;
-
 	if (tree == NULL)
 		return (0);
-	height = binary_tree_height(tree);
-	node = 1;
-	for (i = 0; i < height; i++)
-		node *= 2;
-
-	count = binary_tree_nodes(tree);
-	return ((count == node) ? 1 : 0);
+	return ((is_perfect(tree) != 0) ? 1 : 0);
 }
