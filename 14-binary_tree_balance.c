a857440f61959de585b81f1184073f4c97e373d9
habtew16 <habtamuwolde96@gmail.com>
2023-11-29 10:17:43 -0800

    find the balance factor f the binay tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,41 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+
+/**
+ * max - function to find max of two
+ * numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return largest
+ */
+
+int max(int a, int b)
+{
+	return ((a > b) ? a : b);
+}
+
+
+/**
+ * binary_tree_height - calculate the height of the
+ * binary tree
+ * @tree: bbinary tree
+ * Return: 0 if null else
+ * height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height;
+	size_t r_height;
+
+	if (tree == NULL)
+		return (-1);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	return (1 + max(l_height, r_height));
+}
+
+
 /**
  * binary_tree_balance - function to get binary tee
  * balance
@@ -12,12 +48,7 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l_height;
-	int r_height;
-
 	if (tree == NULL)
 		return (0);
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	return (l_height - r_height);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
