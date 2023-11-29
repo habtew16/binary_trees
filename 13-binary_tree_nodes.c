376e407496aca3fb019c23dac68afe0e00a43527
habtew16 <habtamuwolde96@gmail.com>
2023-11-29 03:46:26 -0800

    binary tree nodes count

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function to count binary tree nodes
+ * @tree: binary tree
+ *
+ * Return: returns 0 if tree is null
+ * or count of nodes
+ */
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (0);
+}
+
